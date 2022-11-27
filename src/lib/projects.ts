fb3b1927f6c55887dc88178a72bc8f11972db3f0
zainsci <60652827+zainsci@users.noreply.github.com>
2022-11-27 18:16:29 +0300

    feat: add projects

---
+++ src/lib/projects.ts
@@ -0,0 +1,182 @@
+export interface Repo {
+	name: string
+	full_name: string
+	description: string
+	id: number
+	github_url: string
+	url: string
+	language: string
+	stars: number
+}
+
+const projects = [
+	{
+		name: "Calendar",
+		full_name: "zainsci/Calendar",
+		description:
+			"Dark-Themed vscode-Like Calendar built with Vanilla JS and Electron",
+		id: 291077284,
+		github_url: "https://github.com/zainsci/Calendar",
+		url: "",
+		language: "Svelte",
+		stars: 4,
+	},
+	{
+		name: "tasklist",
+		full_name: "zainsci/tasklist",
+		description:
+			"A simple PWA Todo app based on the Ivy Lee Method of productivity",
+		id: 513657571,
+		github_url: "https://github.com/zainsci/tasklist",
+		url: "",
+		language: "TypeScript",
+		stars: 0,
+	},
+	{
+		name: "tedit-api",
+		full_name: "zainsci/tedit-api",
+		description:
+			"Social Media Backend built with TypeScript, Express and Prisma",
+		id: 534229240,
+		github_url: "https://github.com/zainsci/tedit-api",
+		url: "",
+		language: "TypeScript",
+		stars: 0,
+	},
+	{
+		name: "tedit-ui",
+		full_name: "zainsci/tedit-ui",
+		description:
+			"Social Media Frontend built with TypeScript, NextJs and Tailwind CSS ",
+		id: 534229468,
+		github_url: "https://github.com/zainsci/tedit-ui",
+		url: "",
+		language: "TypeScript",
+		stars: 0,
+	},
+	{
+		name: "forgext",
+		full_name: "zainsci/forgext",
+		description: "Generate boiler-plate files for your next web-extension",
+		id: 460876037,
+		github_url: "https://github.com/zainsci/forgext",
+		url: "",
+		language: "TypeScript",
+		stars: 0,
+	},
+	{
+		name: "startpage",
+		full_name: "zainsci/startpage",
+		description: "A Startpage for your browser.",
+		id: 289725858,
+		github_url: "https://github.com/zainsci/startpage",
+		url: "",
+		language: "CSS",
+		stars: 2,
+	},
+	{
+		name: "g-reddit",
+		full_name: "zainsci/g-reddit",
+		description:
+			"UI of Gmail with Reddit public api with reddit posts as emails",
+		id: 324366336,
+		github_url: "https://github.com/zainsci/g-reddit",
+		url: "",
+		language: "JavaScript",
+		stars: 0,
+	},
+	{
+		name: "twitter-clone",
+		full_name: "zainsci/twitter-clone",
+		description: "A Twitter Clone Built With React.js",
+		id: 300653317,
+		github_url: "https://github.com/zainsci/twitter-clone",
+		url: "",
+		language: "JavaScript",
+		stars: 0,
+	},
+	{
+		name: "Notes",
+		full_name: "zainsci/Notes",
+		description: "Notes! - A Simple Note Making WebApp.",
+		id: 286690823,
+		github_url: "https://github.com/zainsci/Notes",
+		url: "",
+		language: "JavaScript",
+		stars: 0,
+	},
+	{
+		name: "redux-todo",
+		full_name: "zainsci/redux-todo",
+		description:
+			"A simple todo list application built with react and redux. as a redux practice",
+		id: 369105944,
+		github_url: "https://github.com/zainsci/redux-todo",
+		url: "",
+		language: "JavaScript",
+		stars: 0,
+	},
+	{
+		name: "svelte-todo",
+		full_name: "zainsci/svelte-todo",
+		description:
+			"A simple todo list application for getting familiar with SvelteJs",
+		id: 359449420,
+		github_url: "https://github.com/zainsci/svelte-todo",
+		url: "",
+		language: "JavaScript",
+		stars: 0,
+	},
+	{
+		name: "ramadan",
+		full_name: "zainsci/ramadan",
+		description: "Ramadan Mubarak",
+		id: 357631695,
+		github_url: "https://github.com/zainsci/ramadan",
+		url: "",
+		language: "CSS",
+		stars: 0,
+	},
+	{
+		name: "ramadan-2022",
+		full_name: "zainsci/ramadan-2022",
+		description: "Ramadan Kareem 2022-1443",
+		id: 477199856,
+		github_url: "https://github.com/zainsci/ramadan-2022",
+		url: "",
+		language: "JavaScript",
+		stars: 0,
+	},
+	{
+		name: "eid",
+		full_name: "zainsci/eid",
+		description: "Eid Mubarak",
+		id: 487818748,
+		github_url: "https://github.com/zainsci/eid",
+		url: "",
+		language: "TypeScript",
+		stars: 0,
+	},
+	{
+		name: "v1",
+		full_name: "zainsci/v1",
+		description: "Version one of my portfolio website.",
+		id: 360172744,
+		github_url: "https://github.com/zainsci/v1",
+		url: "",
+		language: "JavaScript",
+		stars: 0,
+	},
+	{
+		name: "vue-blog",
+		full_name: "zainsci/vue-blog",
+		description: "Blog Website with VUE.js",
+		id: 294051090,
+		github_url: "https://github.com/zainsci/vue-blog",
+		url: "",
+		language: "Vue",
+		stars: 0,
+	},
+]
+
+export default projects
